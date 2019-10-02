4fc2b88e2295f7230cf5bb2c2d0590c5f4a0ff5a
monika rani <monika.rani@xebia.com>
2019-10-02 22:53:34 +0530

    test list del function so I added the list del functionalitiy

--- list.py
+++ list.py
@@ -1,6 +1,8 @@
 list = ['abcd',786,2.23,'john',70.2]
 print list
-print list[0]
-print list[1:3]
-list[2] = 'monika'
+# print list[0]
+# print list[1:3]
+# list[2] = 'monika'
+# print list
+del list[2]
 print list
---
+++ math-function.py
@@ -0,0 +1,10 @@
+import math
+import random
+print abs(-2)
+print math.ceil(5.2)
+print math.floor(5.2)
+print cmp(60,50)
+print math.exp(2)
+print random.choice([1,2,3,4])
+print random.choice('moni')
+print 'my name is %s and weight is %d kg !' %('monika',64)
